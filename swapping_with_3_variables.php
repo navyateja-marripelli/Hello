f6240239dc5bf328dbd107126a0149ea8bd463c2
navyateja-marripelli <marripellinavyateja@gmail.com>
2019-06-19 20:42:31 +0530

    Swapping of three variables

---
+++ swapping_with_3_variables.php
@@ -0,0 +1,16 @@
+<?php
+
+// Declaring the variables......
+
+$a = 5;
+$b = 6;
+$c;
+
+echo "Swapping using the third variable \n";
+$c = $a;
+$a = $b;
+$b = $c;
+echo "After swapping we get the values as: \n";
+echo "The value of a is " .$a . "\n";
+echo "The value of b is " .$b . "\n";
+?>
